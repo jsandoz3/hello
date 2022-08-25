439e232db58153b1c683cb483a140624da49db87
jsandoz3 <jsandoz3@huskers.unl.edu>
2022-08-25 22:23:29 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jesse Sandoz
+ * Date: 2022/08/23
+ *
+ * a simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Jesse Sandoz!\n");
+
+    return 0;
+}
